d83d1cd98c8a3ec0ef1c700a2be04e8089c66b95
0112engguh <baguswicaksana217@gmail.com>
2025-11-15 00:31:55 +0700

    First Things

---
+++ resources/js/pages/Products/Create.tsx
@@ -0,0 +1,67 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link, useForm, } from '@inertiajs/react';
+import { CircleAlert } from 'lucide-react';
+import React from 'react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Create a New Product',
+        href: '/products/create',
+    },
+];
+
+export default function Index() {
+
+    const {data, setData, post, processing, errors} = useForm({
+      name: '',
+      price: '',
+      description: ''
+    });
+
+    const handleSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      post('/products');
+    } 
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Create a New Product" />
+            <div className='w-8/12 p-4'>
+              <form onSubmit={handleSubmit} className='space-y-4'>
+                {Object.keys(errors).length > 0 &&(
+                  <Alert>
+                  <CircleAlert />
+                  <AlertTitle>Errors!</AlertTitle>
+                  <AlertDescription>
+                    <ul>
+                      {Object.entries(errors).map(([key, message]) => (
+                        <li key={key}>{message as string}</li>
+                      ))}
+                    </ul>
+                  </AlertDescription>
+                </Alert>
+                )}
+                <div className="gap-1.5">
+                  <Label htmlFor="product name">Name</Label>
+                  <Input placeholder="Product Name" value={data.name} onChange={(e) => setData('name', e.target.value)}></Input>
+                </div>
+                <div className="gap-1.5">
+                  <Label htmlFor="price product">Price</Label>
+                  <Input placeholder="Price" value={data.price} onChange={(e) => setData('price', e.target.value)}></Input>
+                </div>
+                <div className="gap-1.5">
+                  <Label htmlFor="description product">Description</Label>
+                  <Textarea placeholder="Description" value={data.description} onChange={(e) => setData('description', e.target.value)}></Textarea>
+                </div>
+                <Button type="submit">Add Product</Button>
+              </form>
+            </div>
+        </AppLayout>
+    );
+}
